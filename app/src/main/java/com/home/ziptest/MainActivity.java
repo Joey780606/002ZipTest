22204f34dd2115c8bcac8ef6a11129fe320048cf
Joey yang <yty0606@gmail.com>
2020-05-02 11:37:18 +0800

    Build assets folder and place zip file to assets
    1. What issue?
    Try to build assets folder and place zip file to assets
    
    2. Why?
    Our purpose is can put zip into project and send to Mobile's directory.
    
    3. How to do? 
    Reference information and process it. (reference website is in MainActivity.java

--- app/src/main/java/com/home/ziptest/MainActivity.java
+++ app/src/main/java/com/home/ziptest/MainActivity.java
@@ -1,5 +1,12 @@
 package com.home.ziptest;
 
+/**
+ * Created by JoeyYang 2020/05/02
+ *   Reference info:
+ *   1. How to create assets and join zip file into assets
+ *      https://www.youtube.com/watch?v=Xbifoj_x9W4
+ */
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
